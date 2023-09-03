f470a83c11737e5d5e13fc385e5d3ff81f48ff63
Kishan <kshnjshptl@gmail.com>
2023-09-03 17:37:46 -0500

    [Completed] - Chapter 14 and 15 - Javascript Basics

---
+++ Chap14And15_JSBasics/basic.js
@@ -0,0 +1,16 @@
+let product = "artichoke"
+let price = 3.99
+let qty = 4;
+
+//Back ticks allow variables inside strings
+
+`You ${qty} ${product} for ${price*qty}`
+
+
+
+Math
+Math.PI
+Math.round(4.9)
+Math.random()
+Math.floor(Math.random()*10)+1 //1 to 10 (inclusive)
+Math.floor(Math.random()*3)+20 //20 to 22
