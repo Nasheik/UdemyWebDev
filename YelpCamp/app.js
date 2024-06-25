df887b85ef255212cc022cf0abdbbee3fffedc9e
Kishan <kshnjshptl@gmail.com>
2024-06-24 23:57:44 -0400

    [Added] - MongoDB

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,7 +1,21 @@
 const express = require('express');
-const app = express();
-
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require("./Models/campground");
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console,"connection error:"));
+db.once("open", ()=>{
+    console.log("Database Connected")
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'Views'));
@@ -9,6 +23,12 @@ app.set('views', path.join(__dirname, 'Views'));
 app.get('/', (req,res)=> {
     // res.send('Hello From Yelp Camp!')
     res.render('home')
+})
+
+app.get('/makecampground', async (req,res)=> {
+    const camp = new Campground({title: 'My Backyard', description: "Cheap Camping"});
+    await camp.save();
+    res.send(camp)
 })
 
 app.listen(3000,  ()=>{
