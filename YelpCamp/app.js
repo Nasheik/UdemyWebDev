61e2a1d983fcd39b48a43960672e148539bb0531
Kishan <kshnjshptl@gmail.com>
2024-07-12 14:10:32 -0400

    [Added] - Seeding Data [Added] - DB Show All and Show Camp EJS Pages

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -20,15 +20,36 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'Views'));
 
+app.use(express.urlencoded({extended:true}))
+
 app.get('/', (req,res)=> {
     // res.send('Hello From Yelp Camp!')
     res.render('home')
 })
 
-app.get('/makecampground', async (req,res)=> {
-    const camp = new Campground({title: 'My Backyard', description: "Cheap Camping"});
-    await camp.save();
-    res.send(camp)
+// app.get('/makecampground', async (req,res)=> {
+//     const camp = new Campground({title: 'My Backyard', description: "Cheap Camping"});
+//     await camp.save();
+//     res.send(camp)
+// })
+
+app.get("/campgrounds", async(req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('Campgrounds/index', {campgrounds})
+})
+
+app.get("/campgrounds/new", (req,res) => {
+    res.render("campgrounds/new")
+})
+app.post("/campgrounds", async(req,res)=>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get("/campgrounds/:id", async(req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render("campgrounds/show", {campground})
 })
 
 app.listen(3000,  ()=>{
