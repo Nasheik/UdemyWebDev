16c222aa228819f029103634da7514037ba936d2
Kishan <kshnjshptl@gmail.com>
2024-07-20 19:26:09 -0400

    [Refactor] - Routes to separate files [Added] - Stars for Reviews

--- YelpCamp/Routes/reviews.js
+++ YelpCamp/Routes/reviews.js
@@ -6,26 +6,12 @@ const Review = require("../Models/review");
 const catchAsync = require('../Utilities/catchAsync')
 const ExpressError = require('../Utilities/ExpressError');
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
+const reviews = require('../Controllers/reviews');
 
 
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async(req,res)=>{
-    const {id, reviewId} = req.params; 
-    await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', "Succuessly Deleted a Review");
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.post('/',isLoggedIn,validateReview,catchAsync(reviews.createReview))
 
-router.post('/',isLoggedIn,validateReview,catchAsync(async(req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', "Created new Review");
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
