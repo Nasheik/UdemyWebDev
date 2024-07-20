1c7b9365982c09dfc51fd94ed387131e80fd3960
Kishan <kshnjshptl@gmail.com>
2024-07-19 20:51:42 -0400

    [Added] - Authorization to Campgrounds and Reviews Edit and/or Delete

--- YelpCamp/Routes/reviews.js
+++ YelpCamp/Routes/reviews.js
@@ -4,21 +4,12 @@ const {reviewSchema} = require('../schemas')
 const Campground = require("../Models/campground");
 const Review = require("../Models/review");
 const catchAsync = require('../Utilities/catchAsync')
-const ExpressError = require('../Utilities/ExpressError')
+const ExpressError = require('../Utilities/ExpressError');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
 
-const validateReview = (req,res,next)=>{
-    const {error} = reviewSchema.validate(req.body);
-    if(error)
-    {
-        const msg = error.details.map(el=>el.message).join(",");
-        throw new ExpressError(msg, 400);
-    }
-    else next();
-}
 
-
-router.delete('/:reviewId', catchAsync(async(req,res)=>{
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async(req,res)=>{
     const {id, reviewId} = req.params; 
     await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
     await Review.findByIdAndDelete(reviewId);
@@ -26,9 +17,10 @@ router.delete('/:reviewId', catchAsync(async(req,res)=>{
     res.redirect(`/campgrounds/${id}`);
 }))
 
-router.post('/',validateReview,catchAsync(async(req,res)=>{
+router.post('/',isLoggedIn,validateReview,catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
